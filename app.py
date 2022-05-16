39391b74fa9bad387bea053a855a568967c6042e
devz-002 <83504998+devz-002@users.noreply.github.com>
2022-05-16 18:23:04 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,25 @@
+import streamlit as st
+
+#title
+st.title("Hello GeeksForGeeks !!!")
+st.header("This is a header")
+
+#subheader
+st.subheader("This is a subheader")
+
+#text
+st.text("Hello GeeksForGeeks !!!")
+st.success("Success")
+
+#success
+st.info("Information")
+
+#success
+st.warning("Warning")
+
+#success
+st.error("Error")
+st.write("Text with write")
+
+#writing python inbuilt function range
+st.write(range(10))
